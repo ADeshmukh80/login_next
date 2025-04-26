f3c70e0b9f8ba670809bae5f47ea96ba3f48a8ff
ADeshmukh80 <anishadeshmukh6@gmail.com>
2025-04-26 11:32:44 +0530

    first

---
+++ components/LoginForm.jsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import InteractiveBackground from "@/components/InteractiveBackground"; 
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="relative h-screen flex items-center justify-center">
+      <InteractiveBackground />
+
+      <div className="z-10 shadow-lg p-5 rounded-lg border-t-4 border-green-400 bg-white/80 backdrop-blur">
+        <h1 className="text-xl font-bold my-4">Login</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+            className="px-3 py-2 border rounded"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+            className="px-3 py-2 border rounded"
+          />
+          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2 rounded">
+            Login
+          </button>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/register"}>
+           { "Don't have an account?"} <span className="underline">Register</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
